c6c1374de280668a0c229f8e06a2ae002cd3f9d8
Elmosius <2272008@maranatha.ac.id>
2024-10-22 10:20:48 +0700

    Update main_page.dart

--- pertemuan_6/lib/kuis/main_page.dart
+++ pertemuan_6/lib/kuis/main_page.dart
@@ -6,6 +6,7 @@ class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _MainPageState createState() => _MainPageState();
 }
 
